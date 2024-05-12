e2cf1af1307b8d7183c1f353bf6edbbb1fb09c76
ngure808 <mwangingure808@gmail.com>
2024-05-12 14:55:34 +0300

    deployment

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+const cors = require("cors"); // Import the cors library
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = 3000; //  chose port from here like 8080, 3001
+const port = 3000;
 
+// Specify the allowed origin (e.g., your Netlify frontend URL)
+const allowedOrigins = ['https://flavour-haven.netlify.app/'];
+
+// Configure CORS with allowed origins
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+// Apply CORS middleware with options
+server.use(cors(corsOptions));
+
+// Apply other middlewares
 server.use(middlewares);
 server.use(router);
 
